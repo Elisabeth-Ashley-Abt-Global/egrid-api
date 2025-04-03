c87c7789db847d56bc659d11f1d4efd07aa58208
ABT\gofortht <Teagan.Goforth@abtglobal.com>
2025-04-02 21:07:59 -0400

    Deleting commented out section in r_api/call_ files, adding missing columns to modelstest.py

--- backend/egrid/r_api/call_r_nerc.py
+++ backend/egrid/r_api/call_r_nerc.py
@@ -43,37 +43,3 @@ def call_r_nerc():
     except Exception as e:
         logger.error(f"Error while processing R API data: {e}", exc_info=True)
         return {"error": str(e)}
-
- 
-    #     if response.status_code == 200:
-    #         data = response.json()
-    #         if data.get('success'):
-    #             nerc_data = data.get('data', [])
-    #             logger.debug('nerc_data is: ', nerc_data)
-    #             if not nerc_data:
-    #                 logger.warning("R API returned no data. Skipping database update.")
-    #                 return {"success": False, "message": "R API returned no data."}
-
-    #             for item in nerc_data:
-    #                 nerc_region, created = NercRegion.objects.update_or_create(
-    #                     nerc=item.get('nerc'), 
-    #                     defaults={
-    #                         'nerc_name':item.get('nerc'), 
-    #                     }
-    #                 ) 
-
-    #                 if created:
-    #                     logger.info(f"Inserted new plant: {nerc_region.nerc}")
-    #                 else:
-    #                     logger.info(f"Updated existing plant: {nerc_region.nerc}")
-
-    #             return {"success": True, "message": "Data successfully inserted/updated in the Plant table."}
-    #         else:
-    #             logger.error(f"R API returned an error: {data.get('error')}")
-    #             return {"error": f"R API returned an error: {data.get('error')}"}
-    #     else:
-    #         logger.error(f"Failed to connect to R API. Status: {response.status_code}")
-    #         return {"error": f"Failed to connect to R API with status code {response.status_code}"}
-    # except Exception as e:
-    #     logger.error(f"Error while processing R API data: {e}", exc_info=True)
-    #     return {"error": str(e)}
