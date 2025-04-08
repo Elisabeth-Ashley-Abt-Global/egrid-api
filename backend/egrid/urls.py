c202848b11bf6c78e3f53e873153c2bf95cc60a9
Elisabeth Ashley <167812308+Elisabeth-Ashley-Abt-Global@users.noreply.github.com>
2025-04-08 11:18:11 -0400

    removed some views

--- backend/egrid/urls.py
+++ backend/egrid/urls.py
@@ -1,10 +1,6 @@
 from django.urls import path, re_path
 # from .views.balancing_auth_views import r_hello_view, r_ba_view
-from .views.plant_views import ( 
-    FilterPlantsByStateView, 
-    PlantsByYearView,
-    r_plant_view
-)
+ 
 from .views.balancing_auth_views import (
     r_ba_view,
     ba_list_view,
@@ -31,15 +27,15 @@ schema_view = get_schema_view(
 urlpatterns = [
     # path('r-hello/', r_hello_view, name='r_hello'),
     # path('r-process/', r_process_view, name='r_process'),
-    path('r-plant/', r_plant_view, name='r_plant'), 
+    # path('r-plant/', r_plant_view, name='r_plant'), 
     path('r-ba/', r_ba_view, name='r_ba_view'),
     # path('plant/<str:plant_id>/', r_plant_view, name='r_plant'),
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     # path('plants/', plant_views.PlantListView.as_view(), name='plant_list'),
-    path('plants/state/<str:state_id>/', FilterPlantsByStateView.as_view(), name='filter_plants_by_state'),
-    path('plants/year/<int:year>/', PlantsByYearView.as_view(), name='plants_by_year'),
+    # path('plants/state/<str:state_id>/', FilterPlantsByStateView.as_view(), name='filter_plants_by_state'),
+    # path('plants/year/<int:year>/', PlantsByYearView.as_view(), name='plants_by_year'),
     path('balancing-authority/', BalancingAuthorityView.as_view(), name='balancing_authority'),
 
 ]
